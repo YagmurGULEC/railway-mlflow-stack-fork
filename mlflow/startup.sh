27796959a80540151a919b9b668ef1f690a0f37f
gulecy <gulecy@ogsl.ca>
2026-03-03 15:01:55 -0500

    Changed

--- mlflow/startup.sh
+++ mlflow/startup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo 'starting mlflow server...'
 echo "upgrading mlflow db schema..."
-mlflow db upgrade "$DB_URI"
+
 exec mlflow server \
-    --backend-store-uri $DB_URI \
+    --backend-store-uri $DB_URL \
     --artifacts-destination s3://bucket \
     --host :: \
     --port ${PORT:-5000}
