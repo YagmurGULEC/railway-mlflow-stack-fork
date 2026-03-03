b89cba71927514ff4e797965dfa28f33fb7de320
gulecy <gulecy@ogsl.ca>
2026-03-03 14:13:48 -0500

    Changed

--- mlflow/startup.sh
+++ mlflow/startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo 'starting mlflow server...'
 echo "upgrading mlflow db schema..."
-mlflow db upgrade "$DB_URL"
+exec mlflow db upgrade "$DB_URL"
 exec mlflow server \
     --backend-store-uri $DB_URL \
     --artifacts-destination s3://bucket \
